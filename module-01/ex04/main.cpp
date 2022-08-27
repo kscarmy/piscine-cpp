126a70d1092c986298105006485d0ce23277c954
kscarmy <guderram@e1r10p6.clusters.42paris.fr>
2022-08-27 15:59:17 +0200

    M-01 : Push 11 : debut ex04.

---
+++ module-01/ex04/main.cpp
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: guderram <guderram@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/26 15:13:24 by guderram          #+#    #+#             */
+/*   Updated: 2022/08/27 11:38:32 by guderram         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	if (argc != 4){std::cout << "Les comptes sont pas bon" << std::endl; return (0);}
+	argv = argv;
+	return 0;
+}
