ac066ba7f544eead135798eef4b33506a2317864
kscarmy <guderram@e1r13p6.clusters.42paris.fr>
2022-10-06 14:08:52 +0200

    M-05 : Push 31 : ex00 finit.

---
+++ module-05/ex00/main.cpp
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: guderram <guderram@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/26 15:13:24 by guderram          #+#    #+#             */
+/*   Updated: 2022/10/06 14:06:19 by guderram         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+
+int main( void )
+{
+	/***************************************/
+
+	Bureaucrat defo;
+	std::cout << defo.getName() << " has grade " << defo.getGrade() << std::endl;
+	for (int i=0; i<100; i++){
+		defo.increment();
+	}
+	std::cout << defo.getName() << " has grade " << defo.getGrade() << std::endl;
+
+	/***************************************/
+
+	Bureaucrat jean("jean", 0);
+	std::cout << jean.getName() << " has grade " << jean.getGrade() << std::endl;
+	for (int i=0; i<2; i++){
+		jean.decrement();
+	}
+	std::cout << jean.getName() << " has grade " << jean.getGrade() << std::endl;
+
+	std::cout << jean << std::endl;
+	/***************************************/
+
+	// Bureaucrat defo;
+	// std::cout << defo.getName() << " has grade " << defo.getGrade() << std::endl;
+	// for (int i=0; i<100; i++){
+	// 	defo.increment();
+	// }
+	// std::cout << defo.getName() << " has grade " << defo.getGrade() << std::endl;
+
+	/***************************************/
+
+	return 0;
+}
